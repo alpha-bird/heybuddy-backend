b591e3cc39e12bc2e2d7caa973442a162ccb1fa0
alpha-bird <alpha.birdprime@gmail.com>
2018-05-14 23:07:07 +0300

    twitter auth router method changed

--- app/routes/user.js
+++ app/routes/user.js
@@ -19,7 +19,7 @@ module.exports = ( router ) => {
     router.post( '/user/login/google', userController.loginWithGoogle);
     router.post( '/user/login/twitter', userController.loginWithTwitter);
     router.post( '/user/auth/twitter/reverse', userController.reverseTwitter);
-    router.get( '/user/auth/twitter', userController.authTwitter, 
+    router.post( '/user/auth/twitter', userController.authTwitter, 
         passport.authenticate('twitter-token', {session: false}), function(req, res, next) {
             if (!req.user) {
                 return res.send(401, 'User Not Authenticated');
