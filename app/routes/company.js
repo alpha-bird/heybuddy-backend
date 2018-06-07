38f98facc4f66a8053b95cec8b12b66ea2794bc3
alpha-bird <alpha.birdprime@gmail.com>
2018-06-08 00:14:41 +0300

    getting company info

--- app/routes/company.js
+++ app/routes/company.js
@@ -4,4 +4,5 @@ const companyController = require('../controllers/company.controller'),
 module.exports = ( router ) => {
     router.post('/company/create', _AuthCheck, companyController.createCompany);
     router.get('/company/getcompanies', companyController.getAllCompanies);
+    router.post('/company/info', _AuthCheck, companyController.getCompanyInfo);
 }
