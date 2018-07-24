905957640f646326ba8f83bdbc79032ac3904b70
alpha-bird <alpha.birdprime@gmail.com>
2018-07-24 09:06:11 +0300

    backend modified

--- app/routes/buddy.js
+++ app/routes/buddy.js
@@ -5,9 +5,9 @@ module.exports = ( router ) => {
     router.post('/buddy/searchbuddiesbyname', _AuthCheck, buddyController.searchBuddiesByName);
     router.get('/buddy/buddies', _AuthCheck ,buddyController.getAllBuddies);
     router.post('/buddy/buddyavailability', _AuthCheck, buddyController.getBuddyAvailability);
-    router.post('/buddy/mybuddies', _AuthCheck, buddyController.getBuddies);
-    router.post('/buddy/buddyreq', _AuthCheck, buddyController.getBuddyRequests);
-    router.post('/buddy/buddypending', _AuthCheck, buddyController.getBuddyPendings);
+    router.get('/buddy/mybuddies', _AuthCheck, buddyController.getBuddies);
+    router.get('/buddy/buddyreq', _AuthCheck, buddyController.getBuddyRequests);
+    router.get('/buddy/buddypending', _AuthCheck, buddyController.getBuddyPendings);
     router.post('/buddy/sendinvite', _AuthCheck, buddyController.sendInvitation);
     router.post('/buddy/acceptinvite', _AuthCheck, buddyController.acceptInvitation);
     router.post('/buddy/declineinvite', _AuthCheck, buddyController.declineInvitation);
